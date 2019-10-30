70e662b1fd9c899dd6659a59a9f20fce4a762e24
DanyST <lherreral@gmail.com>
2019-10-30 18:44:23 -0300

    Created project, Added .gitignore

---
+++ Midia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midia
+//
+//  Created by Luis Herrera Lillo on 30-10-19.
+//  Copyright © 2019 Luis Herrera Lillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
