a1f6fa9f00fb13d54fa850fcd5e3a3ba199945a2
DanyST <dany.herrera20_@hotmail.com>
2019-10-31 16:15:23 -0300

    Added Book Model

---
+++ Midia/Models/Book.swift
@@ -0,0 +1,23 @@
+//
+//  Book.swift
+//  Midia
+//
+//  Created by Luis Herrera Lillo on 30-10-19.
+//  Copyright © 2019 Luis Herrera Lillo. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    
+    let bookId: String
+    let title: String
+    let authors: [String]?
+    let publishedDate: Date?
+    let description: String?
+    let coverURL: URL?
+    let ratings: Float?
+    let numberOfReviews: Int?
+    let price: Float?
+    
+}
