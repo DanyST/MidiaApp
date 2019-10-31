63a4a083ea7b06fd03a0d2f50fba452222628a90
DanyST <dany.herrera20_@hotmail.com>
2019-10-31 17:14:12 -0300

    Added BookCollection model

---
+++ Midia/Models/BookCollection.swift
@@ -0,0 +1,17 @@
+//
+//  BookCollection.swift
+//  Midia
+//
+//  Created by Luis Herrera Lillo on 31-10-19.
+//  Copyright © 2019 Luis Herrera Lillo. All rights reserved.
+//
+
+import Foundation
+
+struct BookCollection {
+    
+    let kind: String
+    let totalItems: Int
+    let item: [Book]?
+    
+}
