1242df316b6a4e37efcb0e5bd5510a3d095cf856
DanyST <dany.herrera20_@hotmail.com>
2019-11-04 06:26:49 -0300

    Added collection view cell, UI defined, connected outlets

---
+++ Midia/Controllers/MediaItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MediaItemCollectionViewCell.swift
+//  Midia
+//
+//  Created by Luis Herrera Lillo on 04-11-19.
+//  Copyright © 2019 Luis Herrera Lillo. All rights reserved.
+//
+
+import UIKit
+
+class MediaCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
