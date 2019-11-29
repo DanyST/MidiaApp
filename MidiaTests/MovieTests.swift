1c72addb2d89c033292301c2cc7dc3c423573f8b
DanyST <dany.herrera20_@hotmail.com>
2019-11-29 19:47:42 -0300

    Added decoded Movie Collection and Movie test, mapping the keys, for now only movieId and title

--- MidiaTests/MovieTests.swift
+++ MidiaTests/MovieTests.swift
@@ -12,8 +12,10 @@ import XCTest
 class MovieTests: XCTestCase {
     
     var movie: Movie!
+    var decoder: JSONDecoder!
 
     override func setUp() {
+        decoder = JSONDecoder()
         movie = Movie(movieId: 1, title: "Spider-Man: Far From Home")
     }
     
@@ -21,4 +23,24 @@ class MovieTests: XCTestCase {
         XCTAssertNotNil(movie)
     }
     
+    func testDecodeMovieCollection() {
+          guard let path = Bundle(for: type(of: self)).path(forResource: "movie-search-response", ofType: "json") else {
+              XCTFail()
+              return
+          }
+          
+          do {
+              let data = try Data(contentsOf: URL(fileURLWithPath: path))
+              let movieCollection = try decoder.decode(MovieCollection.self, from: data)
+              XCTAssertNotNil(movieCollection)
+              let firstMovie = movieCollection.results?.first
+              XCTAssertNotNil(firstMovie)
+              XCTAssertNotNil(firstMovie?.movieId)
+              XCTAssertNotNil(firstMovie?.title)
+              
+          } catch {
+              XCTFail()
+          }
+      }
+
 }
